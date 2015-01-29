774c3c143ed6e53d7d7bd4d3e775cc2ea3a3eef1
Jeff Porter <jporter8000@gmail.com>
2015-01-29 15:47:45 -0800

    asdf

---
+++ Bidder/src/com/cjproductions/bidder/Impression.java
@@ -0,0 +1,30 @@
+package com.cjproductions.bidder;
+
+public class Impression {
+		private String id;
+		private String displaymanager;
+		private String displaymanagerver;
+		private int instl;
+		private String tagid;
+		private float bidfloor;
+		private String bidfloorcur;
+		
+		public Impression(String id, String displaymanager,
+				String displaymanagerver, int instl, String tagid, float bidfloor,
+				String bidfloorcur) {
+			super();
+			this.id = id;
+			this.displaymanager = displaymanager;
+			this.displaymanagerver = displaymanagerver;
+			this.instl = instl;
+			this.tagid = tagid;
+			this.bidfloor = bidfloor;
+			this.bidfloorcur = bidfloorcur;
+		}
+		
+		// testing 
+		
+		
+	}
+
+
